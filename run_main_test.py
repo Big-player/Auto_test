eee869f85302b284006ecbb3e9e8234d903645cc
ke.cheng <ke.cheng@cloudwise.com>
2022-04-01 16:39:32 +0800

    delete

--- run_main_test.py
+++ run_main_test.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import allure
 
 if __name__ == '__main__':
     pytest.main()
--- testCase/test_01.py
+++ testCase/test_01.py
@@ -1,18 +1,31 @@
-import allure
-import pytest
-import requests
+from locust import HttpUser, task, TaskSet, SequentialTaskSet
+import os
 
-class Test_b():
-    req=[]
-    def test_m1(self,login):
-        url = 'https://docp5-ops.cloudwise.com/gateway/dosm/api/v1/dosm_activiti/workOrder/list/all/filter?currentPage=1&pageSize=30&searchKey=&language=zh&timestamp=1646987147252'
-        data={"createdTime_":{"value":["1639152000~1647014399"],"key":"createdTime","process":"","type":""},"orderTypeList_":{"key":"orderTypeList","process":"","value":["CHANGE"],"type":""}}
-        self.req.append(login)
-        s=login.post(url=url,json=data )
-        print(s.status_code)
 
-    def test_m2(self):
-        url = 'https://docp5-ops.cloudwise.com/gateway/dosm/api/v1/dosm_activiti/workOrder/list/all/filter?currentPage=1&pageSize=30&searchKey=&language=zh&timestamp=1646987147252'
-        data={"createdTime_":{"value":["1639152000~1647014399"],"key":"createdTime","process":"","type":""},"orderTypeList_":{"key":"orderTypeList","process":"","value":["CHANGE"],"type":""}}
-        s=self.req[0].post(url=url,json=data )
-        print(s.json())
+class Test1(SequentialTaskSet):
+    @task
+    def test01(self):
+        print("测试01")
+    @task
+    def test02(self):
+        print("测试02")
+    @task
+    def test03(self):
+        print("测试03")
+    @task
+    def my_task(self):
+        with self.client.get('/v2/zoho/getZohoNetwork', catch_response=True) as re:
+            # print(re.text)
+            if '研发' in re.text:
+                re.success()
+            else:
+                re.failure('fail')
+
+
+class Test(HttpUser):
+    tasks = [Test1]
+    # min_wait = 100
+    # max_wait = 1000
+
+# if __name__ == '__main__':
+#     os.system("locust -f test_01.py --host=http://10.2.2.84:8087")
