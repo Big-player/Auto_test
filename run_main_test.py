6639791013294e953e66e3ee11c04541cf4a186e
ke <80671670+Big-player@users.noreply.github.com>
2022-04-26 22:28:00 +0800

    Update run_main_test.py

--- run_main_test.py
+++ run_main_test.py
@@ -4,6 +4,6 @@ import allure
 if __name__ == '__main__':
     pytest.main()
     # os.system("pytest --alluredir=output")
-    os.system("allure generate output -o report --clean")
+    # os.system("allure generate output -o report --clean")
     #os.system("allure open report")
 
